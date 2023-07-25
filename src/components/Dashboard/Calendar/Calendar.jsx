ad862252fde7f7e28514a4dc83d555696115944b
Janis <janislacis06@gmail.com>
2023-07-25 22:49:33 +0300

    Switch to react-big-calendar

--- src/components/Dashboard/Calendar/Calendar.jsx
+++ src/components/Dashboard/Calendar/Calendar.jsx
@@ -1,37 +1,31 @@
-import {useState} from "react"
-import Calendar from "react-calendar"
-import "react-calendar/dist/Calendar.css"
+import {Calendar, momentLocalizer} from "react-big-calendar"
+import moment from "moment"
+import "react-big-calendar/lib/css/react-big-calendar.css"
 import "./calendar.css"
+import {useState} from "react"
+
+const localizer = momentLocalizer(moment)
 
 const MyCalendar = () => {
-    const calendars = () => {
-        let ansArr = []
-        let date = 0
-        for (let i = 0; i < 12; i++) {
-            const [value, onChange] = useState(new Date(2023, date, 1))
-            ansArr.push(
-                <Calendar
-                    tileClassName="calendar-component"
-                    value={value}
-                    onChange={onChange}
-                    onClickDay={() => console.log("clicked on day")}
-                />
-            )
-            date++
-        }
-        return ansArr
-    }
+    const [date, setDate] = useState(new Date())
 
     return (
         <main className="calendar-main">
-            <div className="calendar-year">
-                {calendars().map((item, index) => {
-                    return (
-                        <div key={index} className="calendar-container">
-                            {item}
-                        </div>
-                    )
-                })}
+            <div className="calendar-container">
+                <Calendar
+                    localizer={localizer}
+                    startAccessor="start"
+                    endAccessor="end"
+                    style={{
+                        height: 500,
+                        backgroundColor: "var(--color-grey-600)",
+                        color: " var(--color-grey-300)",
+                    }}
+                    views={["month"]}
+                    defaultDate={date}
+                    selectable
+                    onNavigate={(newDate) => setDate(newDate)}
+                />
             </div>
         </main>
     )
