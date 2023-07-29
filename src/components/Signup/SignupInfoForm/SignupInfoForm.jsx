80a85bbd0af96691e41539d6704bcb68725be9af
Janis <janislacis06@gmail.com>
2023-07-29 14:23:34 +0300

    Finish users info form (visual)

--- src/components/Signup/SignupInfoForm/SignupInfoForm.jsx
+++ src/components/Signup/SignupInfoForm/SignupInfoForm.jsx
@@ -1,4 +1,106 @@
+import {useState} from "react"
+import userIcon from "../../../assets/user-icon.svg"
+import "./signupinfoform.css"
+
 const SignupInfoForm = () => {
-    return <div>SignupInfoForm</div>
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [username, setUsername] = useState("")
+    const [account, setAccount] = useState("")
+
+    return (
+        <div className="signup-info-form">
+            <h1> hello!</h1>
+            <p>
+                Hello, we need to collect simple information about the user so
+                you can have better experience on our website.
+            </p>
+            <form>
+                <div className="signup-info-form-profile-pic">
+                    <h2>Profile picture (optional)</h2>
+                    <div className="signup-info-form-profile-pic-input">
+                        <input
+                            type="file"
+                            name="profilePic"
+                            id="profilePic"
+                            accept="image/*"
+                        />
+                        <label htmlFor="profilePic">
+                            <img src={userIcon} alt="" />
+                        </label>
+                    </div>
+                </div>
+                <div className="signup-info-form-input-container">
+                    <div class="signup-info-form-field">
+                        <div className="floating">
+                            <input
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                                type="text"
+                                name="firstName"
+                                id="firstName"
+                            />
+                            <label
+                                className={firstName ? "label-up" : ""}
+                                htmlFor="firstName">
+                                First Name:{" "}
+                            </label>
+                        </div>
+                    </div>
+                    <div class="signup-info-form-field">
+                        <div className="floating">
+                            <input
+                                type="text"
+                                name="lastName"
+                                id="lastName"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                            <label
+                                className={lastName ? "label-up" : ""}
+                                htmlFor="lastName">
+                                Your First Name:{" "}
+                            </label>
+                        </div>
+                    </div>
+                    <div class="signup-info-form-field">
+                        <div className="floating">
+                            <input
+                                type="text"
+                                name="username"
+                                id="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            <label
+                                className={username ? "label-up" : ""}
+                                htmlFor="username">
+                                Create Username:{" "}
+                            </label>
+                        </div>
+                    </div>
+                    <div class="signup-info-form-field">
+                        <div className="floating">
+                            <input
+                                type="number"
+                                name="account"
+                                id="account"
+                                value={account}
+                                onChange={(e) => setAccount(e.target.value)}
+                            />
+                            <label
+                                className={account ? "label-up" : ""}
+                                htmlFor="account">
+                                Your Account Balance ($):{" "}
+                            </label>
+                        </div>
+                    </div>
+                    <div className="signup-info-form-field">
+                        <button>Finish</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
 export default SignupInfoForm
